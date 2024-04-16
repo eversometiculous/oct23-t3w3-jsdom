3d4071ade58c5f700e027ce7442c3c96107ef5d0
eversometiculous <126760003+eversometiculous@users.noreply.github.com>
2024-04-16 20:53:21 +1000

    implemented JS colour schemes for CSS variables

---
+++ themeControl.js
@@ -0,0 +1,24 @@
+const themes = {
+	light: {
+		background: "white",
+		color: "darkgrey"
+	},
+	dark: {
+		background: "darkgrey",
+		color: "#ffffff"
+	}
+};
+
+function changeCssTheme(themeName){
+	// themes[themeName].variable
+	for (const variable in themes[themeName]){
+		document.documentElement.style.setProperty(`--${variable}`, themes[themeName][variable]);
+		console.log("Updated CSS variable --" + variable);
+
+	}
+}
+
+changeCssTheme("dark");
+
+console.log('Hello from Nick\'s code');
+console.log("Hello from Nick's code");
